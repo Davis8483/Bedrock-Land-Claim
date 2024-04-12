a335591863a15b91b648509b8a0950036d7d6ba1
Noah Davis <76828148+Davis8483@users.noreply.github.com>
2024-04-11 23:43:34 -0500

    Claim Management
    wip, added a menu to edit/delete claims

--- BP/scripts/main.ts
+++ BP/scripts/main.ts
@@ -13,60 +13,202 @@ var database = JSON.parse(world.getDynamicProperty("db").toString());
 
 function saveDb() {
     world.setDynamicProperty("db", JSON.stringify(database));
-};
+}
+
+
 
 function sendNotification(player: Player, langEntry: String) {
     player.runCommandAsync(`tellraw @s {"rawtext":[{"translate":"chat.prefix"}, {"text":" "}, {"translate":"${langEntry}"}]}`);
 }
 
-function createClaim(player: Player, start: [x: number, y: number, z: number], end: [x: number, y: number, z: number]) {
-    var claims: {} = database[player.name]["claims"];
+class Ui {
+    static main(player: Player) {
+        const form = new ActionFormData()
+            .title("ui.main:title")
+            .button("ui.main:manage_claims_button")
 
-    const form = new ModalFormData()
-        .title("New Claim")
-        // .body("Crazy, I was crazy once. They locked me in a room, a rubber room. A rubber room with rats, and rats make me crazy.")
-        .textField("Name", "Ex: Home")
-        .toggle("Public Access", true)
+        form.show(player).then((response) => {
+            if (response.selection === 0) {
+                this.manage(player);
+            };
+        });
+    }
 
-    form.show(player).then((response) => {
+    static newClaim(player: Player, start: [x: number, y: number, z: number], end: [x: number, y: number, z: number]) {
+        var claims: {} = database[player.name]["claims"];
 
-        if (!response.canceled) {
+        const form = new ModalFormData()
+            .title("ui.claim.new:title")
+            // .body("Crazy, I was crazy once. They locked me in a room, a rubber room. A rubber room with rats, and rats make me crazy.")
+            .textField("ui.claim:name", "ui.claim:name_placeholder")
+            .toggle("ui.claim:public_toggle", true)
 
-            var name = response.formValues[0].toString();
-            var hasPublicAccess = response.formValues[1];
+        form.show(player).then((response) => {
 
-            if (name.length == 0) {
-                sendNotification(player, "chat.claim:name_required")
-                player.playSound("note.didgeridoo");
-            }
-            else if (name in claims) {
-                sendNotification(player, "chat.claim:use_unique_name")
-                player.playSound("note.didgeridoo");
-            }
+            if (!response.canceled) {
 
-            else {
-                // generate dict for the new claim
-                claims[name] = {
-                    "start": start,
-                    "end": end,
+                var name = response.formValues[0].toString();
+                var hasPublicAccess = response.formValues[1];
 
-                    "public": {
-                        "access": hasPublicAccess,
-                        "permisions": {
-                            "break-blocks": false,
-                            "place-blocks": false
-                        }
-                    },
-                    "whitelist": {}
-
+                if (name.length == 0) {
+                    sendNotification(player, "chat.claim:name_required")
+                    player.playSound("note.didgeridoo");
                 }
-                sendNotification(player, "chat.claim:created")
-                player.playSound("random.levelup");
-            }
-        }
-        saveDb();
+                else if (name in claims) {
+                    sendNotification(player, "chat.claim:use_unique_name")
+                    player.playSound("note.didgeridoo");
+                }
 
-    });
+                else {
+                    // generate dict for the new claim
+                    claims[name] = {
+                        "start": start,
+                        "end": end,
+
+                        "public": {
+                            "access": hasPublicAccess,
+                            "permisions": {
+                                "break-blocks": false,
+                                "place-blocks": false
+                            }
+                        },
+                        "whitelist": {}
+
+                    }
+                    sendNotification(player, "chat.claim:created")
+                    player.playSound("random.levelup");
+                }
+            }
+            saveDb();
+
+        });
+    }
+
+    static manage(player: Player) {
+        var claims = Object.keys(database[player.name]["claims"]);
+
+        const form = new ActionFormData()
+            .title("ui.manage:title")
+
+        for (var c of claims) {
+            form.button(c);
+        }
+
+        form.button("ui:back_button")
+
+        form.show(player).then((response) => {
+            if (response.selection == claims.length) {
+                // return to previous menu
+                this.main(player);
+            }
+            else {
+                this.manageClaim(player, claims[response.selection].toString());
+            }
+        });
+    }
+
+    static manageClaim(player: Player, claim: string) {
+        var claims = Object.keys(database[player.name]["claims"]);
+
+        const form = new ActionFormData()
+            .title(claim)
+            .button("ui.manage.public_permissions_button")
+            .button("ui.manage.whitelist_button")
+            .button("ui.manage.rename_button")
+            .button("ui.manage.delete_claim_button")
+            .button("ui:back_button")
+
+        form.show(player).then((response) => {
+            if (response.selection == 0) {
+
+            }
+            else if (response.selection == 1) {
+                this.publicPermissions(player, claim);
+            }
+            else if (response.selection == 2) {
+                this.renameClaim(player, claim);
+            }
+            else if (response.selection == 3) {
+                this.deleteClaim(player, claim);
+            }
+            else if (response.selection == 4) {
+                // return to previous menu
+                this.manage(player);
+            }
+        });
+    }
+
+    static deleteClaim(player: Player, claim: string) {
+        var claims: {} = database[player.name]["claims"];
+
+        const form = new MessageFormData()
+            .title(claim)
+            .body("ui.manage.delete:body")
+            .button1("ui.manage.delete:cancel_button")
+            .button2("ui.manage.delete:confirm_button")
+
+        form.show(player).then((response) => {
+            // if deletion canceled
+            if (response.selection == 0) {
+
+                // return to previous page on menu
+                this.manageClaim(player, claim);
+            }
+            else if (response.selection == 1) {
+
+                // delete claim
+                delete claims[claim];
+                sendNotification(player, "chat.claim:deleted")
+                player.playSound("mob.creeper.say");
+
+                saveDb();
+            }
+        });
+    }
+
+    static renameClaim(player: Player, claim: string) {
+        var claims: {} = database[player.name]["claims"];
+
+        const form = new ModalFormData()
+            .title("Rename Claim")
+            // .body("Crazy, I was crazy once. They locked me in a room, a rubber room. A rubber room with rats, and rats make me crazy.")
+            .textField("ui.claim:name", "ui.claim:name_placeholder")
+
+        form.show(player).then((response) => {
+
+            if (!response.canceled) {
+
+                var name = response.formValues[0].toString();
+
+                if (name.length == 0) {
+                    sendNotification(player, "chat.claim:name_required")
+                    player.playSound("note.didgeridoo");
+                }
+                else if (name in claims) {
+                    sendNotification(player, "chat.claim:use_unique_name")
+                    player.playSound("note.didgeridoo");
+                }
+
+                else {
+
+                    // copy the claim over to the new name key
+                    claims[name] = Object.assign({}, claim[claim]);
+
+                    // delete the old name key
+                    delete claims[claim];
+
+                    sendNotification(player, "chat.claim:renamed")
+                    player.playSound("note.cow_bell");
+                }
+            }
+            saveDb();
+
+        });
+    }
+
+    static publicPermissions(player: Player, claim: string) {
+
+    }
 }
 
 world.afterEvents.playerJoin.subscribe((data) => {
@@ -111,15 +253,7 @@ world.afterEvents.playerSpawn.subscribe((data) => {
 world.afterEvents.itemUse.subscribe((data) => {
 
     if (data.itemStack.typeId == shovelID) {
-        const form = new ActionFormData()
-            .title("Land Claim Menu")
-            .button("Manage Claims")
-
-        form.show(data.source).then((response) => {
-            if (response.selection === 0) {
-                world.sendMessage("Truly cringe");
-            };
-        });
+        Ui.main(data.source);
     };
 });
 
@@ -161,7 +295,7 @@ world.beforeEvents.playerBreakBlock.subscribe((data) => {
             world.sendMessage("Second point selected");
 
             system.run(() => {
-                createClaim(data.player, [firstPoint["x"], firstPoint["y"], firstPoint["z"]], [data.block.x, data.block.y, data.block.z]);
+                Ui.newClaim(data.player, [firstPoint["x"], firstPoint["y"], firstPoint["z"]], [data.block.x, data.block.y, data.block.z]);
             });
 
         }
